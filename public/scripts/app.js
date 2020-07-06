849d7cf04accd313e1067fe3417ba9f2582ebe14
CJ Roberts <cjroberts2013@gmail.com>
2020-07-05 23:21:24 -0500

    More practice problems

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,17 @@
 "use strict";
 
 console.log('app.js is running');
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hello I am CJ"), /*#__PURE__*/React.createElement("p", null, "This is a blank statement to fill up some space!!!"));
+var title = 'Welcome to Texas!';
+var subtitle = 'We are happy to have you.';
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, title), /*#__PURE__*/React.createElement("p", null, subtitle));
+var user = {
+  name: 'Christina',
+  age: 25,
+  location: 'Novi'
+};
+var userName = 'Cole';
+var userAge = 25;
+var userLocation = 'Austin';
+var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : Anonymous), /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), /*#__PURE__*/React.createElement("p", null, "Location: ", user.location));
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 console.log('app.js is running')
 
+var title = 'Welcome to Texas!'
+var subtitle = 'We are happy to have you.'
+
 var template = (
     <div>
-        <h1>Hello I am CJ</h1>
-        <p>This is a blank statement to fill up some space!!!</p>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
     </div>
 );
+
+var user = {
+    name: 'Christina',
+    age: 25,
+    location: 'Novi'
+}
+
+var userName = 'Cole';
+var userAge = 25;
+var userLocation = 'Austin'
+
+var template2 = (
+    <div>
+        <h1>{user.name ? user.name : Anonymous}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot)
