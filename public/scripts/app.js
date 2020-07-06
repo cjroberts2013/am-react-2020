58e688b16626ed5734f259d782566094a4e86c44
CJ Roberts <cjroberts2013@gmail.com>
2020-07-06 09:45:21 -0500

    Practice with form submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,23 @@
 "use strict";
 
-var _this = void 0;
-
-console.log('app.js is running');
-var title = 'Welcome to Texas!';
-var subtitle = 'We are happy to have you.';
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, title), /*#__PURE__*/React.createElement("p", null, subtitle));
-var user = {
-  name: 'Christina',
-  age: 25,
-  location: 'Novi'
-};
-var userName = 'Cole';
-var userAge = 25;
-var userLocation = 'Austin';
-var count = 0;
-
-var addOne = function addOne() {
-  _this.count++;
-};
-
-var subOne = function subOne() {
-  _this.count--;
-};
-
-var reset = function reset() {
-  count = 0;
-};
-
-var template2 = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Count: ", count), /*#__PURE__*/React.createElement("button", {
-  onClick: addOne
-}, "+1"), /*#__PURE__*/React.createElement("button", {
-  onClick: subOne
-}, "-1"), /*#__PURE__*/React.createElement("button", {
-  onClick: reset
-}, "-1"));
-console.log(count); //ARROW FUNCTION PRACTICE
+// console.log('app.js is running')
+// const title = 'Welcome to Texas!'
+// const subtitle = 'We are happy to have you.'
+// const template = (
+//     <div>
+//         <h1>{title}</h1>
+//         <p>{subtitle}</p>
+//     </div>
+// );
+// const user = {
+//     name: 'Christina',
+//     age: 25,
+//     location: 'Novi'
+// }
+// const userName = 'Cole';
+// const userAge = 25;
+// const userLocation = 'Austin'
+// ARROW FUNCTION PRACTICE //
 // const multiplier = {
 //     numbers: [1, 2, 3, 4, 5],
 //     multiplyBy: 2,
@@ -44,6 +26,75 @@ console.log(count); //ARROW FUNCTION PRACTICE
 //     }
 // }
 // console.log(multiplier.multiply())
+// PRACTICE DATA BINDINGING //
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+// const subOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
+// const renderCounterApp = () => {
+//     const template2 = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={subOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
+//     console.log(count);
+//     ReactDOM.render(template2, appRoot)
+// };
+// renderCounterApp();
+var app = {
+  title: 'This is my App',
+  subtitle: 'Put your life in the hands of a computer.',
+  options: []
+};
 
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderForm();
+  }
+};
+
+var empty = function empty() {
+  app.options = [];
+  renderForm();
+};
+
+var user = {
+  name: 'Christina',
+  age: 25,
+  location: 'Novi'
+};
+var userName = 'Cole';
+var userAge = 25;
+var userLocation = 'Austin';
 var appRoot = document.getElementById('app');
-ReactDOM.render(template2, appRoot);
+
+var renderForm = function renderForm() {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Form"), /*#__PURE__*/React.createElement("p", null, "Fill in data below"), /*#__PURE__*/React.createElement("p", null, app.options.length), /*#__PURE__*/React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), /*#__PURE__*/React.createElement("button", null, "Add Option")), /*#__PURE__*/React.createElement("button", {
+    onClick: empty
+  }, "Empty options"));
+  ReactDOM.render(template, appRoot);
+};
+
+renderForm();
--- src/app.js
+++ src/app.js
@@ -1,49 +1,31 @@
-console.log('app.js is running')
+// console.log('app.js is running')
 
-const title = 'Welcome to Texas!'
-const subtitle = 'We are happy to have you.'
+// const title = 'Welcome to Texas!'
+// const subtitle = 'We are happy to have you.'
 
-const template = (
-    <div>
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-    </div>
-);
+// const template = (
+//     <div>
+//         <h1>{title}</h1>
+//         <p>{subtitle}</p>
+//     </div>
+// );
 
-const user = {
-    name: 'Christina',
-    age: 25,
-    location: 'Novi'
-}
+// const user = {
+//     name: 'Christina',
+//     age: 25,
+//     location: 'Novi'
+// }
 
-const userName = 'Cole';
-const userAge = 25;
-const userLocation = 'Austin'
+// const userName = 'Cole';
+// const userAge = 25;
+// const userLocation = 'Austin'
 
 
 
-let count = 0;
-const addOne = () => {
-    this.count++
-}
-const subOne = () => {
-    this.count--
-}
-const reset = () => {
-    count = 0
-}
 
-const template2 = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={subOne}>-1</button>
-        <button onClick={reset}>-1</button>
-    </div>
-);
-console.log(count)
 
-//ARROW FUNCTION PRACTICE
+
+// ARROW FUNCTION PRACTICE //
 
 // const multiplier = {
 //     numbers: [1, 2, 3, 4, 5],
@@ -57,7 +39,96 @@ console.log(count)
 
 
 
+
+// PRACTICE DATA BINDINGING //
+
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+// const subOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
+
+// const renderCounterApp = () => {
+//     const template2 = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={subOne}>-1</button>
+//             <button onClick={reset}>reset</button>
+//         </div>
+//     );
+//     console.log(count);
+
+//     ReactDOM.render(template2, appRoot)
+// };
+
+// renderCounterApp();
+
+const app = {
+    title: 'This is my App',
+    subtitle: 'Put your life in the hands of a computer.',
+    options: []
+}
+
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderForm()
+    }
+}
+
+const empty = () => {
+    app.options = [];
+    renderForm()
+}
+
+
+const user = {
+    name: 'Christina',
+    age: 25,
+    location: 'Novi'
+}
+
+const userName = 'Cole';
+const userAge = 25;
+const userLocation = 'Austin'
+
+
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot)
 
+
+
+const renderForm = () => {
+    const template = (
+        <div>
+            <h1>Form</h1>
+            <p>Fill in data below</p>
+            <p>{app.options.length}</p>
+            <form onSubmit={onFormSubmit}>
+                <input type='text' name='option' />
+                <button>Add Option</button>
+            </form>
+            <button onClick={empty}>Empty options</button>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot)
+}
+
+
+renderForm()
