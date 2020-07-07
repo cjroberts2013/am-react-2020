2ec372098a5f0fbbde260b537be9ab3fc0793c6b
CJ Roberts <cjroberts2013@gmail.com>
2020-07-07 15:04:36 -0500

    Working with more classes and extending class properties

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,25 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -9,10 +29,11 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 var Person = /*#__PURE__*/function () {
   function Person() {
     var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
     _classCallCheck(this, Person);
 
-    this.name = name;
+    this.name = name, this.age = age;
   }
 
   _createClass(Person, [{
@@ -20,10 +41,53 @@ var Person = /*#__PURE__*/function () {
     value: function getGreeting() {
       return "Hello ".concat(this.name);
     }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      return "".concat(this.name, " is ").concat(this.age, " years old!");
+    }
   }]);
 
   return Person;
 }();
 
-var me = new Person('CJ Roberts');
-console.log(me.getGreeting());
+var Student = /*#__PURE__*/function (_Person) {
+  _inherits(Student, _Person);
+
+  var _super = _createSuper(Student);
+
+  function Student(name, age, major) {
+    var _this;
+
+    _classCallCheck(this, Student);
+
+    _this = _super.call(this, name, age);
+    _this.major = major;
+    return _this;
+  }
+
+  _createClass(Student, [{
+    key: "hasMajor",
+    value: function hasMajor() {
+      return !!this.major;
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      var description = _get(_getPrototypeOf(Student.prototype), "getDescription", this).call(this);
+
+      if (this.hasMajor()) {
+        description += " ".concat(this.name.split(' ')[0], " is majoring in ").concat(this.major, ".");
+      }
+
+      return description;
+    }
+  }]);
+
+  return Student;
+}(Person);
+
+var me = new Student('CJ Roberts', 25, 'Biochemistry');
+var anon = new Student();
+console.log(me.getDescription());
+console.log(anon.getDescription());
--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
 
-
 class Person {
-    constructor(name = 'Anonymous') {
-        this.name = name
+    constructor(name = 'Anonymous', age = 0) {
+        this.name = name,
+            this.age = age
     }
     getGreeting() {
         return `Hello ${this.name}`
     }
+    getDescription() {
+        return `${this.name} is ${this.age} years old!`
+    }
 }
 
-const me = new Person('CJ Roberts');
-console.log(me.getGreeting())
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` ${this.name.split(' ')[0]} is majoring in ${this.major}.`;
+        }
+
+        return description
+    }
+}
+
+const me = new Student('CJ Roberts', 25, 'Biochemistry');
+const anon = new Student();
+
+console.log(me.getDescription());
+console.log(anon.getDescription());
