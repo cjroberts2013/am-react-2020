f88339e3e0855be91207037cacfdb16b53825e66
CJ Roberts <cjroberts2013@gmail.com>
2020-07-11 17:22:31 -0500

    Changed some components to staeless functional components

--- src/app.js
+++ src/app.js
@@ -40,12 +40,11 @@ class IndecisionApp extends React.Component {
     }
 
     render() {
-        const title = "Indecision";
         const subtitle = "Put your life in the hands of a computer"
 
         return (
             <div>
-                <Header title={title} subtitle={subtitle} />
+                <Header subtitle={subtitle} />
                 <Action
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick} />
@@ -59,47 +58,44 @@ class IndecisionApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        )
-    }
+
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
+        </div>
+    )
 }
 
-class Action extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.handlePick} disabled={!this.props.hasOptions} >What should I do?</button>
-            </div>
-        )
-    }
+Header.defaultProps = {
+    title: 'Indecision'
 }
 
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.handleDeleteOptions} >Remove All Options</button>
-                <div>
-                    {this.props.options.map((x, i) => <Option key={i} optionText={x} />)}
-                </div>
-            </div>
-        )
-    }
+const Action = (props) => {
+    return (
+        <div>
+            <button onClick={props.handlePick} disabled={!props.hasOptions} >What should I do?</button>
+        </div>
+    )
 }
 
-class Option extends React.Component {
-    render() {
-        return (
-            <div>{this.props.optionText}</div>
-        )
-    }
+const Options = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions} >Remove All Options</button>
+            <div>
+                {props.options.map((x, i) => <Option key={i} optionText={x} />)}
+            </div>
+        </div>
+    )
+}
+
+const Option = (props) => {
+    return (
+        <div>{props.optionText}</div>
+    )
 }
 
 // Set up form with input and submit
